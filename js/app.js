80a81d22b0c3cb02fb75111431e415f1d2e87465
Danny <dannybcreative@gmail.com>
2019-09-17 09:38:05 -0700

    Approved and ready for WP migration

--- js/app.js
+++ js/app.js
@@ -27,3 +27,34 @@ const desktopNav = () => {
 const mobileNav = () => {
   mobileHeader.appendChild(navBar);
 }
+
+/* Animation */
+const mindWrapper = document.querySelector('.mind'),
+  mindBox = mindWrapper.querySelector('.section-info'),
+  productWrapper = document.querySelector('.product-wrapper'),
+  productBox = productWrapper.querySelectorAll('.product-box'),
+  winHeight = window.innerHeight;
+
+window.addEventListener('scroll', () => {
+
+  if (mindWrapper.getBoundingClientRect().top < winHeight / 6) {
+    mindBox.classList.add('show');
+  } else {
+    mindBox.classList.remove('show');
+  }
+
+
+  if (productWrapper.getBoundingClientRect().top < winHeight / 3) {
+    productBox.forEach((staff, i) => {
+      setTimeout(() => {
+        staff.classList.add("show");
+      }, i * 350);
+    });
+  } else {
+    productBox.forEach((staff, i) => {
+      setTimeout(() => {
+        staff.classList.remove("show");
+      }, i * 350);
+    });
+  }
+});
